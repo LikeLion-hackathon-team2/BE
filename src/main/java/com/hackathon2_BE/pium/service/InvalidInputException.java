6aeb97179c4b6e572816d6e8315052bf9e0f41f8
danieljjs <danieljjs77@gmail.com>
2025-08-10 10:29:19 +0900

    초기 상태

--- src/main/java/com/hackathon2_BE/pium/service/InvalidInputException.java
+++
@@ -1,8 +0,0 @@
-package com.hackathon2_BE.pium.service;
-
-public class InvalidInputException extends RuntimeException {
-    public InvalidInputException(String message) {
-        super(message);
-    }
-}
-
--- src/main/java/com/hackathon2_BE/pium/service/UsernameAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.hackathon2_BE.pium.service;
-
-public class UsernameAlreadyExistsException extends RuntimeException {
-    public UsernameAlreadyExistsException(String message) {
-        super(message);
-    }
-}
