42a3439af589552e567200c2d27bbba2c0ea4cf7
forwoni <ktion0619@naver.com>
2025-08-11 15:20:53 +0900

    feat: Product 엔티티 추가

--- src/main/java/com/hackathon2_BE/pium/entity/Product.java
+++ src/main/java/com/hackathon2_BE/pium/entity/Product.java
@@ -1,7 +1,33 @@
 package com.hackathon2_BE.pium.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "product")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    private Long userId;
+    private Long gradeId;
+    private Long categoryId;
+
+    private String name;
+    private String info;
+
+    private Integer price;
+    private Integer stockQuantity;
+
+    private LocalDateTime createdAt;
+
 }
