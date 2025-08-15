ccfaedb101608ff1e87f16b094401b55f4e1fa35
danieljjs <danieljjs77@gmail.com>
2025-08-16 03:25:04 +0900

    회원가입 최종

--- src/main/java/com/hackathon2_BE/pium/controller/UserController.java
+++ src/main/java/com/hackathon2_BE/pium/controller/UserController.java
@@ -1,15 +1,20 @@
 package com.hackathon2_BE.pium.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.hackathon2_BE.pium.dto.UserDTO;
-import com.hackathon2_BE.pium.dto.ApiResponse;
+import com.hackathon2_BE.pium.dto.ApiResponse; // ⬅ 추가
 import com.hackathon2_BE.pium.dto.MeResponse;
-import com.hackathon2_BE.pium.dto.UserSignupResponse; // ⬅ 추가
+import com.hackathon2_BE.pium.dto.UserDTO;
+import com.hackathon2_BE.pium.dto.UserSignupResponse;
 import com.hackathon2_BE.pium.entity.User;
 import com.hackathon2_BE.pium.service.UserService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/user")
@@ -27,9 +32,9 @@ public class UserController {
     }
 
     @GetMapping("/me")
-    public ResponseEntity<?> me() {
+    public ResponseEntity<ApiResponse<MeResponse>> me() {
         MeResponse me = userService.getMe();  // 내부에서 현재 사용자 추출
         return ResponseEntity.status(201)
-                .body(new ApiResponse<>(true, "CREATED", "내 정보 조회 성공", me));
+                .body(new ApiResponse<>(true, "OK", "내 정보 조회 성공", me));
     }
 }
