056a59b785c12f5552e1b82567ffde269079e7ca
DESKTOP-TE9JGFD\danie <danieljjs77@gmail.com>
2025-08-24 18:29:33 +0900

    내 정보조회 공동구매 개인주문 추가

--- src/main/java/com/hackathon2_BE/pium/entity/GroupPurchaseParticipant.java
+++ src/main/java/com/hackathon2_BE/pium/entity/GroupPurchaseParticipant.java
@@ -43,4 +43,6 @@ public class GroupPurchaseParticipant {
 
     @CreationTimestamp
     @Column(name = "joined_at", updatable = false)
-    private LocalDateTime joinedAt;}
+    private LocalDateTime joinedAt;
+
+}
